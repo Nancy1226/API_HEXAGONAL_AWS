44707140c38b911b9c8bbd9e39a32b4d1536f23a
Nancy1226 <nancy-12_03@hotmail.com>
2024-07-03 13:50:01 -0600

    [UPDATE] env

--- src/user/infrastructure/databases/mysql/mysql.ts
+++ src/user/infrastructure/databases/mysql/mysql.ts
@@ -1,11 +1,23 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
+// Cargar variables de entorno
+dotenv.config();
+
+// Asegurarse de que las variables de entorno están definidas
+for (const key of ['MYSQL_HOST', 'MYSQL_PORT', 'MYSQL_USER', 'MYSQL_PASSWORD', 'MYSQL_DATABASE']) {
+  if (!process.env[key]) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+}
+
+// Convertir MYSQL_PORT de string a number
 const config = {
-  host: 'database-api-hexagonal.cdf69l49jvum.us-east-1.rds.amazonaws.com',
-  port: 3306,  // Asegúrate de usar el puerto correcto para MySQL
-  user: 'admin', 
-  password: 'myDBhexagonal21',
-  database: 'storage_hexagonalBD',  // Este es el nombre de la base de datos que definiste al crear la instancia de RDS
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),  // Asegurarse de que port es un número
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 };
 
 export const query = async (sql: string, params: any[]) => {
